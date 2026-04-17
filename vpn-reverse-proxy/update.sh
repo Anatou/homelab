180589cc9ca0e27dc4574dd29af7161bc67f5102
Anatou <anatole@desnot.com>
2026-04-17 12:10:48 +0200

    reverse-proxy bug fixes

--- vpn-reverse-proxy/update.sh
+++ vpn-reverse-proxy/update.sh
@@ -1,1 +1,1 @@
-curl -L https://github.com/Anatou/homelab/raw/refs/heads/main/vpn-reverse-proxy/Caddyfile > Caddyfile
+rm Caddyfile && curl -L https://github.com/Anatou/homelab/raw/refs/heads/main/vpn-reverse-proxy/Caddyfile > Caddyfile
