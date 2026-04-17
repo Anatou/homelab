22c1e076dddf8ddcc81e19be386fffb35ab3867e
Anatou <anatole@desnot.com>
2026-04-17 09:51:10 +0200

    fixed homelab scripts

--- homepage/update.sh
+++ homepage/update.sh
@@ -1,7 +1,3 @@
-wget https://github.com/Anatou/homelab/raw/refs/heads/main/homepage/proxmox.yaml
-wget https://github.com/Anatou/homelab/raw/refs/heads/main/homepage/services.yaml
-wget https://github.com/Anatou/homelab/raw/refs/heads/main/homepage/settings.yaml
-
-mv proxmox.yaml /opt/homepage/config/proxmox.yaml
-mv services.yaml /opt/homepage/config/services.yaml
-mv settings.yaml /opt/homepage/config/settings.yaml
+curl -L https://github.com/Anatou/homelab/raw/refs/heads/main/homepage/proxmox.yaml > /opt/homepage/config/proxmox.yaml
+curl -L https://github.com/Anatou/homelab/raw/refs/heads/main/homepage/services.yaml > /opt/homepage/config/services.yaml
+curl -L https://github.com/Anatou/homelab/raw/refs/heads/main/homepage/settings.yaml > /opt/homepage/config/settings.yaml
